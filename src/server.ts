a7cfb43d297d1742670b8abf4adae3147a82c68d
Mehad Hossen <mehad004938@gmail.com>
2026-06-25 09:44:48 +0600

    finally running the project

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const port:any=process.env.PORT || 5000;
 async function main() {
     try {
         app.listen(port,()=>{
-            console.log(`the server is on port : ${port}`)
+            console.log(`the server is running on port : ${port}`)
 
         })
 
