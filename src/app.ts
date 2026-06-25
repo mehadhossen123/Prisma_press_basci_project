665014fe3e3ef6ef202d85c32f55a6486c45bc45
Mehad Hossen <mehad004938@gmail.com>
2026-06-25 10:56:09 +0600

    setup some middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app:Application=express()
 
@@ -8,6 +9,10 @@ app.use(cors({
     origin:config.app_url,
     credentials:true
 }))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
 
 
 
