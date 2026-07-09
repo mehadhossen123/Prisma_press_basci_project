5c51a53f754171adf15d8a972f9812e4ffba516e
Mehad Hossen <mehad004938@gmail.com>
2026-07-09 09:28:03 +0600

    setup stripe class

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,13 +3,14 @@ import dotenv, { config } from "dotenv"
 dotenv.config({path:path.join(process.cwd(),".env")})
 
 export default {
-    port:process.env.PORT,
-    database_url:process.env.DATABASE_URL,
-    app_url:process.env.APP_URL,
-    bcrypt_salt_rounds:process.env.BCRYPT_SALT_ROUNDS,
-    jWt_access_secret:process.env.JWT_ACCESS_SECRET!,
-    jwt_refresh_secret:process.env.JWT_REFRESH_SECRET!,
-    jWt_access_expires_in:process.env.JWT_ACCESS_EXPIRES_IN!,
-    jwt_refresh_expires_in:process.env.JWT_REFRESH_EXPIRES_IN!,
-
-}
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  app_url: process.env.APP_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jWt_access_secret: process.env.JWT_ACCESS_SECRET!,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+  jWt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+  stripe_product_id: process.env.STRIPE_PRODUCT_ID,
+  stripe_secret_key:process.env.STRIPE_SECRET_KEY
+};
